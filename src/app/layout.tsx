add5bd81f4461e53328a25406caf63ee014e9eef
Connor Littleton <connorlittleton@wealthfront.com>
2024-12-29 15:19:05 -0800

    get keyboard navigation working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,50 @@
-import type { Metadata, Viewport } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
 
-import { getAllPlaylists } from '@/lib/db/queries'
+import { getAllPlaylists } from "@/lib/db/queries";
 
-import './globals.css'
+import "./globals.css";
 
-import { NowPlaying } from '@/components/now-playing'
-import { PlaybackControls } from '@/components/playback-controls'
-import { Playlists } from '@/components/playlists'
-import { PlaybackProvider } from '@/context/playback-context'
-import { PlaylistProvider } from '@/context/playlist-context'
+import { NowPlaying } from "@/components/now-playing";
+import { PlaybackControls } from "@/components/playback-controls";
+import { Playlists } from "@/components/playlists";
+import { PlaybackProvider } from "@/context/playback-context";
+import { PlaylistProvider } from "@/context/playlist-context";
 
 export const metadata: Metadata = {
-  title: 'CWL-UI | Music Player',
-  description: 'A local first music player',
-}
+	title: "CWL-UI | Music Player",
+	description: "A local first music player",
+};
 
 export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  viewportFit: 'cover',
-  themeColor: '#0A0A0A',
-}
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	viewportFit: "cover",
+	themeColor: "#0A0A0A",
+};
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"], preload: false });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  const playlistsPromise = getAllPlaylists()
+	const playlistsPromise = getAllPlaylists();
 
-  return (
-    <html lang="en" className={inter.className}>
-      <body className="dark flex flex-col md:flex-row h-[100dvh] text-gray-200 bg-[#0A0A0A]">
-        <PlaybackProvider>
-          <PlaylistProvider playlistsPromise={playlistsPromise}>
-            <Playlists />
-            {children}
-          </PlaylistProvider>
-          <NowPlaying />
-          <PlaybackControls />
-        </PlaybackProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" className={inter.className}>
+			<body className="dark flex flex-col md:flex-row h-[100dvh] text-gray-200 bg-[#0A0A0A]">
+				<PlaybackProvider>
+					<PlaylistProvider playlistsPromise={playlistsPromise}>
+						<Playlists />
+						{children}
+					</PlaylistProvider>
+					<NowPlaying />
+					<PlaybackControls />
+				</PlaybackProvider>
+			</body>
+		</html>
+	);
 }
---
+++ src/app/p/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default async function PlaylistPage({
+	params,
+}: { params: Promise<{ id: string }> }) {
+	return (
+		<div className="flex-1 flex flex-col overflow-hidden bg-[#0A0A0A] pb-[69px]">
+			PlaylistPage
+		</div>
+	);
+}
