6f297e41353bbb35d945e017441b148e459d58ff
Connor Littleton <connorlittleton@wealthfront.com>
2024-12-30 16:11:57 -0800

    Make some more updates

--- src/components/playlists.tsx
+++ src/components/playlists.tsx
@@ -1,118 +1,135 @@
-'use client'
+"use client";
 
-import React, { useRef } from 'react'
-import { MoreVertical, Trash } from 'lucide-react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import React, { useRef } from "react";
+import { MoreVertical, Trash } from "lucide-react";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
-import { Button } from '@/components/button'
-import { CreatePlaylistModal } from '@/components/create-playlist-modal'
-import { SearchInput } from '@/components/search-input'
-import { usePlayback } from '@/context/playback-context'
-import { usePlaylist } from '@/context/playlist-context'
-import { addPlaylistAction } from '@/lib/actions'
-import { Playlist } from '@/lib/db/types'
-import { cn } from '@/lib/utils'
+import { Button } from "@/components/button";
+import { CreatePlaylistModal } from "@/components/create-playlist-modal";
+import { SearchInput } from "@/components/search-input";
+import { usePlayback } from "@/context/playback-context";
+import { usePlaylist } from "@/context/playlist-context";
+import { Playlist } from "@/lib/db/types";
+import { cn } from "@/lib/utils";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from './dropdown-menu'
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "./dropdown-menu";
+import { deletePlaylistAction } from "@/lib/actions";
+import { ScrollArea, ScrollBar } from "./scroll-area";
 
 export function Playlists() {
-  const pathname = usePathname()
-  const { playlists } = usePlaylist()
-  const playlistsContainerRef = useRef<HTMLDivElement>(null)
-  const { registerPanelRef, setActivePanel, handleKeyNavigation } = usePlayback()
+	const pathname = usePathname();
+	const { playlists } = usePlaylist();
+	const playlistsContainerRef = useRef<HTMLDivElement>(null);
+	const { registerPanelRef, setActivePanel, handleKeyNavigation } =
+		usePlayback();
 
-  React.useEffect(() => {
-    registerPanelRef('sidebar', playlistsContainerRef)
-  }, [registerPanelRef])
+	React.useEffect(() => {
+		registerPanelRef("sidebar", playlistsContainerRef);
+	}, [registerPanelRef]);
 
-  return (
-    <div
-      ref={playlistsContainerRef}
-      className="hidden md:block w-56 bg-[#121212] h-[100dvh] overflow-auto"
-      onClick={() => setActivePanel('sidebar')}
-    >
-      <div className="m-4">
-        <SearchInput />
-        <div className="mb-6">
-          <Link
-            href="/"
-            className={cn(
-              'block py-1 px-4 -mx-4 text-xs text-[#d1d5db] hover:bg-[#1A1A1A] transition-colors focus:outline-none focus:ring-1 focus:ring-gray-400',
-              pathname === '/' ? 'bg-[#1A1A1A]' : '',
-            )}
-          >
-            All Tracks
-          </Link>
-        </div>
-        <div className="flex justify-between items-center mb-4">
-          <Link
-            href="/"
-            className="focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white rounded-sm text-xs font-semibold text-gray-400 hover:text-white transition-colors"
-          >
-            Playlists
-          </Link>
-          <form action={addPlaylistAction}>
-            <CreatePlaylistModal />
-          </form>
-        </div>
-      </div>
-      <ul
-        className="space-y-0.5 text-xs mt-[1px]"
-        onKeyDown={(e) => handleKeyNavigation('sidebar', e)}
-      >
-        {playlists.map((playlist) => (
-          <PlaylistRow key={playlist.name} playlist={playlist} />
-        ))}
-      </ul>
-    </div>
-  )
+	return (
+		<div
+			ref={playlistsContainerRef}
+			className="hidden md:block w-56 bg-[#121212]"
+			onClick={() => setActivePanel("sidebar")}
+		>
+			<div className="m-4">
+				<SearchInput />
+				<div className="mb-6">
+					<Link
+						href="/"
+						className={cn(
+							"block py-1 px-4 -mx-4 text-xs text-[#d1d5db] hover:bg-[#1A1A1A] transition-colors focus:outline-none focus:ring-1 focus:ring-gray-400",
+							pathname === "/" ? "bg-[#1A1A1A]" : "",
+						)}
+					>
+						All Tracks
+					</Link>
+				</div>
+				<div className="flex justify-between items-center mb-4">
+					<Link
+						href="/"
+						className="focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white rounded-sm text-xs font-semibold text-gray-400 hover:text-white transition-colors"
+					>
+						Playlists
+					</Link>
+					<CreatePlaylistModal />
+				</div>
+			</div>
+			<ScrollArea className="h-[calc(100dvh-220px)]">
+				<ul
+					className="space-y-0.5 text-xs mt-[1px]"
+					onKeyDown={(e) => handleKeyNavigation("sidebar", e)}
+				>
+					{playlists.map((playlist) => (
+						<PlaylistRow key={playlist.name} playlist={playlist} />
+					))}
+				</ul>
+				<ScrollBar orientation="vertical" />
+			</ScrollArea>
+		</div>
+	);
 }
 
 function PlaylistRow({ playlist }: { playlist: Playlist }) {
-  const pathname = usePathname()
+	const pathname = usePathname();
+	const router = useRouter();
 
-  async function handleDeletePlaylist() {
-    // call deletePlaylist action
-  }
+	async function handleDeletePlaylist() {
+		try {
+			await deletePlaylistAction(playlist.id);
 
-  return (
-    <li className="relative group">
-      <Link
-        prefetch={true}
-        href={`/p/${playlist.id}`}
-        className={cn(
-          'block py-1 px-4 text-xs text-[#d1d5db] hover:bg-[#1A1A1A] transition-colors focus:outline-none focus:ring-1 focus:ring-gray-400',
-          pathname === `/p/${playlist.id}` ? 'bg-[#1A1A1A]' : '',
-        )}
-        tabIndex={0}
-      >
-        {playlist.name}
-      </Link>
-      <div className="absolute right-2 top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="h-6 w-6 text-gray-400 hover:text-white focus:text-white"
-            >
-              <MoreVertical className="h-4 w-4" />
-              <span className="sr-only">Playlist options</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="w-36">
-            <DropdownMenuItem className="text-xs" onClick={() => handleDeletePlaylist()}>
-              <Trash className="mr-2 size-3" />
-              Delete Playlist
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </li>
-  )
+			if (pathname === `/p/${playlist.id}`) {
+				router.push("/");
+			}
+
+			router.refresh();
+		} catch (error) {
+			// TODO: handle error
+		}
+	}
+
+	return (
+		<li className="relative group">
+			<Link
+				prefetch={true}
+				href={`/p/${playlist.id}`}
+				className={cn(
+					"block py-1 px-4 text-xs text-[#d1d5db] hover:bg-[#1A1A1A] transition-colors focus:outline-none focus:ring-1 focus:ring-gray-400",
+					pathname === `/p/${playlist.id}` ? "bg-[#1A1A1A]" : "",
+				)}
+				tabIndex={0}
+			>
+				{playlist.name}
+			</Link>
+			<div className="absolute right-2 top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100">
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button
+							variant="ghost"
+							size="icon"
+							className="h-6 w-6 text-gray-400 hover:text-white focus:text-white"
+						>
+							<MoreVertical className="h-4 w-4" />
+							<span className="sr-only">Playlist options</span>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end" className="w-36">
+						<DropdownMenuItem
+							className="text-xs"
+							onClick={() => handleDeletePlaylist()}
+						>
+							<Trash className="mr-2 size-3" />
+							Delete Playlist
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+		</li>
+	);
 }
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,48 +1,65 @@
-'use server'
+"use server";
 
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
-import { db } from '@/lib/db/drizzle'
-import { DEFAULT_COVER_URL } from './db/constants'
-import { playlists } from './db/schema'
+import { db } from "@/lib/db/drizzle";
+import { DEFAULT_COVER_URL } from "./db/constants";
+import { playlists, playlistSongs } from "./db/schema";
+import { revalidateTag } from "next/cache";
+import { eq } from "drizzle-orm";
 
 export async function addPlaylistAction() {
-  const newPlaylistId = uuidv4()
-  const newPlaylist = {
-    id: newPlaylistId,
-    name: 'New Playlist',
-    coverUrl: '',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }
-  // TODO:
-  // 1. Update playlist
-  // 2. Prefetch next route
-  // 3. Push route
+	const newPlaylistId = uuidv4();
+	const newPlaylist = {
+		id: newPlaylistId,
+		name: "New Playlist",
+		coverUrl: "",
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	};
+	// TODO:
+	// 1. Update playlist
+	// 2. Prefetch next route
+	// 3. Push route
 }
 
-export async function createPlaylistAction(_: any, formData: FormData) {
-  const name = formData.get('playlist-name') as string
+export async function createPlaylistAction(formData: FormData) {
+	// TODO: refactor these actions to just handle server stuff and blow up
+	// if an issue happens. Validation should happen prior to this and error handling
+	// can be done on the client.
+	const name = formData.get("playlist-name") as string;
 
-  if (!name) {
-    throw new Error('Missing playlist name')
-  }
+	if (!name) {
+		throw new Error("Missing playlist name");
+	}
 
-  const playlistId = uuidv4()
+	const playlistId = uuidv4();
 
-  try {
-    // TODO: need to add cover photo here as well.
-    // In development we'll use some random unsplash photo or something
-    // But in production we'll need to save this somewhere like in S3.
-    await db.insert(playlists).values({
-      id: playlistId,
-      name,
-      coverUrl: DEFAULT_COVER_URL,
-    })
+	try {
+		// TODO: need to add cover photo here as well.
+		// In development we'll use some random unsplash photo or something
+		// But in production we'll need to save this somewhere like in S3.
+		await db.insert(playlists).values({
+			id: playlistId,
+			name,
+			coverUrl: DEFAULT_COVER_URL,
+		});
+		revalidateTag("playlists");
 
-    return { success: true, playlistId: playlistId }
-  } catch (error) {
-    console.error('Error uploading file:', error)
-    throw new Error('Failed to add new Playlist')
-  }
+		return { success: true, playlistId: playlistId };
+	} catch (error) {
+		console.error("Error uploading file:", error);
+		throw new Error("Failed to add new Playlist");
+	}
+}
+
+export async function deletePlaylistAction(id: string) {
+	await db.transaction(async (tx) => {
+		await tx
+			.delete(playlistSongs)
+			.where(eq(playlistSongs.playlistId, id))
+			.execute();
+
+		await tx.delete(playlists).where(eq(playlists.id, id)).execute();
+	});
 }
